f590b659f5406f5fc5955b86e3c55c88eee7787f
Ishaan Chaturvedi <ishaan27chaturvedi@gmail.com>
2021-12-19 00:17:54 +0000

    Add files via upload

---
+++ weights.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 18 20:59:01 2021
+
+@author: ishaanchaturvedi
+"""
+
+
+
+{'pos': 0.5604387813165689,
+ 'prefix': 0.5839230503175057,
+ 'prev_tok': 0.5979146527551892,
+ 'prev_3_tok': 0.5028554450014276,
+ 'prev_2_tok': 0.5410048348714536,
+ 'prev_tok_tag2': 0.6133407806352734,
+ 'prev_tok_tag5': 0.6026734745808369,
+ 'prev_tok_tag4': 0.5935353619377447,
+ 'prev_tag_1_2': 0.6276249674830447,
+ 'prev_tag_1_3': 0.6350483300525643}
+
+{'pos': 0.5604387813165689,
+ 'prefix': 0.5839230503175057,
+ 'prev_tok': 0.5979146527551892,
+ 'prev_3_tok': 0.5028554450014276,
+ 'prev_2_tok': 0.5410048348714536,
+ 'prev_tok_tag2': 0.6133407806352734,
+ 'prev_tok_tag5': 0.6026734745808369,
+ 'prev_tok_tag4': 0.5935353619377447,
+ 'prev_tag_1_2': 0.6276249674830447,
+ 'prev_tag_1_3': 0.6350483300525643,
+ 'prev_tag_1_4': 0.6130301084444342,
+ 'prev_tag_1_3_test': 0.6350483300525643,
+ 'prev_tag_1_3_post_tag_1': 0.6093860883841885,
+ 'prev_tag_1_3_post_tag_1_2': 0.613936892387406,
+ 'prev_tag_1_3_post_word_1_2': 0.6324702713146589,
+ 'prev_tag_1_3_post_word_1': 0.6300664194291152,
+ 'prev_tag_1_3_post_word_1_5': 0.619634839224073,
+ 'prev_tag_1_3_post_word_1_3': 0.6310579108460682,
+ 'prev_tag_1_3x': 0.6350483300525643}
+
+
+
+
+'prev_tag_1_3x_suf4': 0.6332714784251957,
+ 'prev_tag_1_3x': 0.6350483300525643,
+ 'prev_tag_1_3x_remove_suffix_3': 0.6221435529115016,
+ 'prev_tag_1_3x_remove_prefix_3': 0.6258858872727605,
+ 'prev_tag_1_3x_stop': 0.6342256219821321,
+ 'prev_tag_1_3x_prevstop': 0.6364624276338356,
+ 'prev_tag_1_3x_prev_curr_stop': 0.6338416145489008,
+ 'prev_tag_1_3x_prev_stop_curr_stem': 0.6292423462500897,
+ 'prev_tag_1_3x_prev_stop_prev_stem': 0.6247374485764525,
+ 'prev_tag_1_3x_prev_stop_prev_stemm': 0.628626656441372,
+ 'prev_tag_1_3x_prev_stop_curr_stemm': 0.6410088526382691,
+ 'prev_tag_1_3x_prev_stop_curr_stemm_lemm': 0.6356312552863791,
+ 'prev_tag_1_3x_prev_stop_curr_stemm_lemmm': 0.6293157030215604,
+ 'prev_tag_1_3x_prev_stop_curr_stemmy': 0.6410088526382691,
+ 'prev_tag_1_3x_prev_stop_curr_prev_stemmy': 0.6263247335911132,
+ 'prev_tag_1_3x_prev_stop_curr_stemm_prev_lemm': 0.6225199300128967,
+ 'prev_tag_1_3x_prev_stop_curr_stemm_len': 0.6320088102564206,
+ 'prev_tag_1_3x_prev_stop_curr_stemm_len_curr': 0.6410688365174024,
+ 'prev_tag_1_3x_prev_stop_curr_stemm_len_curr_firstlast': 0.6433143283854416,
+ 'prev_tag_1_3x_prev_stop_curr_stemm_len_curr_firstlast_prev_suff': 0.6248797063095859,
+ 'prev_tag_1_3x_prev_stop_curr_stemm_len_curr_firstlast_X': 0.6426146062284641,
+ 'prev_tag_1_3x_prev_stop_curr_stemm_len_curr_firstlast_X2': 0.6433143283854416,
+ 'prev_tag_1_3x_prev_stop_curr_stemm_len_curr_firstlast_X3': 0.6433143283854416,
+ 'X3_mindoc2': 0.6425055330391296,
+ 'X3_mindoc1': 0.6433143283854416}
+
+
+
+
